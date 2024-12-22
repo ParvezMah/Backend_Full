2ddab61ac4d33b3687ba132fb2e9d609cb362994
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-12-22 06:48:58 +0600

    Login Logic in user.controller.js

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './db/database.js';
+import userRoute from "./routes/user.route.js";
+import bodyParser from 'body-parser';
 dotenv.config();
 
 const app = express();
 
 connectDB();
 
-const PORT=3000;
+const PORT=process.env.PORT || 3000;
+
+
+// Middleware
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use("/api/v1/user", userRoute);
 
 app.listen(PORT, ()=>{
-    console.log(`Server lisen at port ${PORT}`);
+    console.log(`Server listen at port ${PORT}`);
 })
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,7 @@
-import { User } from "../models/user";
+import { User } from "../models/user.js";
 import bcrypt from "bcrypt";
 
-async (req, res)=> {
+export const register = async (req, res)=> {
     try {
         const {fullname, email, password}=req.body;
         if(!fullname || !email || !password){
@@ -12,7 +12,7 @@ async (req, res)=> {
         }
 
         // finding the user is existed or not
-        const user = await User.findOne(email);
+        const user = await User.findOne({email});
         if(user){
             return res.status(403).json({
                 success: false,
@@ -33,6 +33,43 @@ async (req, res)=> {
         })
         
     } catch (error) {
-        
+        console.log("Error is Register Logic : ", error)
+    }
+}
+
+
+export const login = async (req, res)=>{
+    try {
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(403).json({
+                success: false,
+                message: "All fields are required."
+            })
+        }
+
+        // finding the user is existed or not
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(403).json({
+                success: false,
+                message:"Incorrect email.",
+            })
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if(!isPasswordMatch){
+            return res.status(403).json({
+                success: false,
+                message:"Incorrect password.",
+            })
+        }
+
+        return res.status(201).json({
+            success: true,
+            message: `Welcome Back ${user.fullname}`
+        })
+    } catch (error) {
+        console.log("Error is Login Logic : ", error)
     }
 }
